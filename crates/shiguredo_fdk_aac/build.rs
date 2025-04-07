7c569d1136ce4955a719eed532395a35de98fd4d
Takeru Ohta <phjgt308@gmail.com>
2025-04-07 11:03:01 +0900

    Update build.rs

--- crates/shiguredo_fdk_aac/build.rs
+++ crates/shiguredo_fdk_aac/build.rs
@@ -11,38 +11,17 @@ fn main() {
     // 各種変数やビルドディレクトリのセットアップ
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").expect("infallible"));
     let out_build_dir = out_dir.join("build/");
-    let src_dir = out_build_dir.join(LIB_NAME);
-    let output_lib_dir = src_dir.join(".libs/");
     let output_bindings_path = out_dir.join("bindings.rs");
     let _ = std::fs::remove_dir_all(&out_build_dir);
     std::fs::create_dir(&out_build_dir).expect("failed to create build directory");
 
     // バインディングを生成する
     bindgen::Builder::default()
-        // TODO: .clang_arg(format!("-I{}", src_dir.join("libSYS/include/").display()))
-        .header(
-            src_dir
-                .join("libSYS/include/machine_type.h")
-                .display()
-                .to_string(),
-        )
-        .header(
-            src_dir
-                .join("libSYS/include/FDK_audio.h")
-                .display()
-                .to_string(),
-        )
-        .header(
-            src_dir
-                .join("libAACenc/include/aacenc_lib.h")
-                .display()
-                .to_string(),
-        )
+        .header("/usr/include/fdk-aac/aacenc_lib.h")
         .generate()
         .expect("failed to generate bindings")
         .write_to_file(output_bindings_path)
         .expect("failed to write bindings");
 
-    println!("cargo::rustc-link-search={}", output_lib_dir.display());
     println!("cargo::rustc-link-lib={LIB_NAME}");
 }
