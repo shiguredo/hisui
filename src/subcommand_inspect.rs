4cd80d84f6c74047cb07eba7b1085ec60e7c9c73
Takeru Ohta <phjgt308@gmail.com>
2025-06-10 11:30:58 +0900

    音声系のサマリ情報を結果のトップレベルに追加

--- src/subcommand_inspect.rs
+++ src/subcommand_inspect.rs
@@ -80,8 +80,10 @@ pub fn run<P: AsRef<Path>>(input_file_path: P) -> orfail::Result<()> {
         path: input_file_path.as_ref().to_path_buf(),
         format,
         audio_codec,
-        video_codec,
+        audio_duration: audio_samples.iter().map(|s| s.duration).sum(),
+        audio_sample_count: audio_samples.len(),
         audio_samples,
+        video_codec,
     };
     println!(
         "{}",
@@ -100,9 +102,10 @@ struct FileInfo {
     path: PathBuf,
     format: ContainerFormat,
     audio_codec: Option<CodecName>,
-    video_codec: Option<CodecName>,
-    // TODO: sample_count, duration
+    audio_duration: Duration,
+    audio_sample_count: usize,
     audio_samples: Vec<AudioSampleInfo>,
+    video_codec: Option<CodecName>,
 }
 
 impl nojson::DisplayJson for FileInfo {
@@ -112,12 +115,12 @@ impl nojson::DisplayJson for FileInfo {
             f.member("format", &self.format)?;
             if let Some(c) = self.audio_codec {
                 f.member("audio_codec", c)?;
+                f.member("audio_duration_us", self.audio_duration.as_micros())?;
+                f.member("audio_sample_count", self.audio_sample_count)?;
+                f.member("audio_samples", &self.audio_samples)?;
             }
             if let Some(c) = self.video_codec {
                 f.member("video_codec", c)?;
-            }
-            if !self.audio_samples.is_empty() {
-                f.member("audio_samples", &self.audio_samples)?;
             }
             Ok(())
         })
