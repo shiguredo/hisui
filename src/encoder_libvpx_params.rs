2d40c0eb41fb1a92309cb3e8c8f9c42f757189da
Takeru Ohta <phjgt308@gmail.com>
2025-07-07 11:21:07 +0900

    vp8 用のエンコードパラメータパースを追加

--- src/encoder_libvpx_params.rs
+++ src/encoder_libvpx_params.rs
@@ -29,103 +29,67 @@ pub fn parse_libvpx_vp8_encode_params(
             "best" => Ok(shiguredo_libvpx::EncodingDeadline::Best),
             "good" => Ok(shiguredo_libvpx::EncodingDeadline::Good),
             "realtime" => Ok(shiguredo_libvpx::EncodingDeadline::Realtime),
-            _ => Err(v.invalid("")),
+            _ => Err(v.invalid("unknown 'deadline' value")),
         })?
     {
         config.deadline = deadline;
     }
 
-    // // レート制御モード
-    // if let Some(rate_control) = params.get("rate_control") {
-    //     config.rate_control = match rate_control.to_str()? {
-    //         "vbr" => shiguredo_libvpx::RateControlMode::Vbr,
-    //         "cbr" => shiguredo_libvpx::RateControlMode::Cbr,
-    //         "cq" => shiguredo_libvpx::RateControlMode::Cq,
-    //         _ => return Err(nojson::JsonParseError::InvalidValue),
-    //     };
-    // }
+    // レート制御モード
+    if let Some(rate_control) = params.get_with("rate_control", |v| {
+        match v.to_unquoted_string_str()?.as_ref() {
+            "vbr" => Ok(shiguredo_libvpx::RateControlMode::Vbr),
+            "cbr" => Ok(shiguredo_libvpx::RateControlMode::Cbr),
+            "cq" => Ok(shiguredo_libvpx::RateControlMode::Cq),
+            _ => Err(v.invalid("unknown 'rate_control' value")),
+        }
+    })? {
+        config.rate_control = rate_control;
+    }
 
-    // // 先読みフレーム数
-    // if let Some(lag_in_frames) = params.get("lag_in_frames") {
-    //     if let Ok(lag) = std::num::NonZeroUsize::new(lag_in_frames.to_u64()? as usize) {
-    //         config.lag_in_frames = Some(lag);
-    //     }
-    // }
+    // 先読みフレーム数
+    config.lag_in_frames = params.get("lag_in_frames")?;
 
-    // // スレッド数
-    // if let Some(threads) = params.get("threads") {
-    //     if let Ok(thread_count) = std::num::NonZeroUsize::new(threads.to_u64()? as usize) {
-    //         config.threads = Some(thread_count);
-    //     }
-    // }
+    // スレッド数
+    if let Some(threads) = params.get("threads")? {
+        if let Some(thread_count) = std::num::NonZeroUsize::new(threads) {
+            config.threads = Some(thread_count);
+        }
+    }
 
-    // // エラー耐性モード
-    // if let Some(error_resilient) = params.get("error_resilient") {
-    //     config.error_resilient = error_resilient.to_bool()?;
-    // }
+    // エラー耐性モード
+    config.error_resilient = params.get("error_resilient")?.unwrap_or_default();
 
-    // // キーフレーム間隔
-    // if let Some(keyframe_interval) = params.get("keyframe_interval") {
-    //     if let Ok(interval) = std::num::NonZeroUsize::new(keyframe_interval.to_u64()? as usize) {
-    //         config.keyframe_interval = Some(interval);
-    //     }
-    // }
+    // キーフレーム間隔
+    config.keyframe_interval = params.get("keyframe_interval")?;
 
-    // // フレームドロップ閾値
-    // if let Some(frame_drop_threshold) = params.get("frame_drop_threshold") {
-    //     config.frame_drop_threshold = Some(frame_drop_threshold.to_u64()? as usize);
-    // }
+    // フレームドロップ閾値
+    config.frame_drop_threshold = params.get("frame_drop_threshold")?;
 
-    // // 以降はVP8固有の設定
-    // let mut vp8_config = shiguredo_libvpx::Vp8Config {
-    //     noise_sensitivity: None,
-    //     static_threshold: None,
-    //     token_partitions: None,
-    //     max_intra_bitrate_pct: None,
-    //     arnr_config: None,
-    // };
+    // 以降はVP8固有の設定
+    let mut vp8_config = shiguredo_libvpx::Vp8Config {
+        noise_sensitivity: None,
+        static_threshold: None,
+        token_partitions: None,
+        max_intra_bitrate_pct: None,
+        arnr_config: None,
+    };
+    vp8_config.noise_sensitivity = params.get("noise_sensitivity")?;
+    vp8_config.static_threshold = params.get("static_threshold")?;
+    vp8_config.token_partitions = params.get("token_partitions")?;
+    vp8_config.max_intra_bitrate_pct = params.get("max_intra_bitrate_pct")?;
 
-    // if let Some(noise_sensitivity) = params.get("noise_sensitivity") {
-    //     vp8_config.noise_sensitivity = Some(noise_sensitivity.to_i64()? as i32);
-    // }
+    // ARNR設定
+    vp8_config.arnr_config = params.get_with("arnr_config", |v| {
+        let arnr_obj = JsonObject::new(v)?;
+        Ok(shiguredo_libvpx::ArnrConfig {
+            max_frames: arnr_obj.get("max_frames")?.unwrap_or(0) as i32,
+            strength: arnr_obj.get("strength")?.unwrap_or(3) as i32,
+            filter_type: arnr_obj.get("filter_type")?.unwrap_or(1) as i32,
+        })
+    })?;
 
-    // if let Some(static_threshold) = params.get("static_threshold") {
-    //     vp8_config.static_threshold = Some(static_threshold.to_i64()? as i32);
-    // }
-
-    // if let Some(token_partitions) = params.get("token_partitions") {
-    //     vp8_config.token_partitions = Some(token_partitions.to_i64()? as i32);
-    // }
-
-    // if let Some(max_intra_bitrate_pct) = params.get("max_intra_bitrate_pct") {
-    //     vp8_config.max_intra_bitrate_pct = Some(max_intra_bitrate_pct.to_i64()? as i32);
-    // }
-
-    // // ARNR設定
-    // if let Some(arnr_params) = params.get("arnr_config") {
-    //     let arnr_obj = arnr_params.to_object()?;
-    //     let arnr_config = shiguredo_libvpx::ArnrConfig {
-    //         max_frames: arnr_obj
-    //             .get("max_frames")
-    //             .map(|v| v.to_i64())
-    //             .transpose()?
-    //             .unwrap_or(0) as i32,
-    //         strength: arnr_obj
-    //             .get("strength")
-    //             .map(|v| v.to_i64())
-    //             .transpose()?
-    //             .unwrap_or(3) as i32,
-    //         filter_type: arnr_obj
-    //             .get("filter_type")
-    //             .map(|v| v.to_i64())
-    //             .transpose()?
-    //             .unwrap_or(1) as i32,
-    //     };
-    //     vp8_config.arnr_config = Some(arnr_config);
-    // }
-
-    // config.vp8_config = Some(vp8_config);
-
+    config.vp8_config = Some(vp8_config);
     Ok(config)
 }
 
