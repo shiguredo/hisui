66a217847ac8888d549ea0f6d5f7d9cd867ca2c3
Takeru Ohta <phjgt308@gmail.com>
2025-07-07 13:18:16 +0900

    cfg(macos) 漏れ対応

--- src/layout_encode_params.rs
+++ src/layout_encode_params.rs
@@ -10,7 +10,9 @@ pub struct LayoutEncodeParams {
     pub libvpx_vp9: Option<shiguredo_libvpx::EncoderConfig>,
     pub openh264: Option<shiguredo_openh264::EncoderConfig>,
     pub svt_av1: Option<shiguredo_svt_av1::EncoderConfig>,
+    #[cfg(target_os = "macos")]
     pub video_toolbox_h264: Option<shiguredo_video_toolbox::EncoderConfig>,
+    #[cfg(target_os = "macos")]
     pub video_toolbox_h265: Option<shiguredo_video_toolbox::EncoderConfig>,
 }
 
@@ -35,11 +37,13 @@ impl<'text> nojson::FromRawJsonValue<'text> for LayoutEncodeParams {
                 "svt_av1_encode_params" => {
                     params.svt_av1 = Some(encoder_svt_av1_params::parse_encode_params(value)?);
                 }
+                #[cfg(target_os = "macos")]
                 "video_toolbox_h264_encode_params" => {
                     params.video_toolbox_h264 = Some(
                         encoder_video_toolbox_params::parse_h264_encode_params(value)?,
                     );
                 }
+                #[cfg(target_os = "macos")]
                 "video_toolbox_h265_encode_params" => {
                     params.video_toolbox_h265 = Some(
                         encoder_video_toolbox_params::parse_h265_encode_params(value)?,
