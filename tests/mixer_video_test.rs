d603eb699136b387447af038e7b279294630345a
Takeru Ohta <phjgt308@gmail.com>
2025-08-12 11:26:51 +0900

    複数リージョンでのソース共有時にリサイズ処理が正しく動作することを検証するテストを追加

--- tests/mixer_video_test.rs
+++ tests/mixer_video_test.rs
@@ -342,6 +342,128 @@ fn single_source_multiple_regions() {
     });
 }
 
+/// 一つのソースを複数のリージョンで使用するテストのリサイズあり版
+#[test]
+fn single_source_multiple_regions_with_resize() {
+    let error_flag = ErrorFlag::new();
+    let stats = SharedStats::new();
+    let (input_tx, input_rx) = channel::sync_channel_with_bound(1000);
+    let total_duration = ms(1000);
+
+    // 各種サイズ
+    let output_size = size(MIN_OUTPUT_WIDTH, MIN_OUTPUT_HEIGHT);
+    let region_size = size(12, 12);
+    let cell_size = size(12, 10);
+
+    // ソースは一つだけ
+    let source = source(0, ms(0), total_duration); // 1000 ms 分のソース
+
+    // ソースを共有する二つのリージョン設定
+    let mut region0 = region(region_size, cell_size);
+    region0.source_ids.insert(source.id.clone());
+    region0.position.x = EvenUsize::truncating_new(2); // 一つ目のリージョンの描画位置は端から 2 pixel 分ずらす
+    region0.position.y = EvenUsize::truncating_new(2);
+    region0.top_border_pixels = EvenUsize::truncating_new(0); // こっちは上限の枠線はなし
+    region0.grid.rows = 1;
+    region0.grid.columns = 1;
+    region0.grid.assign_source(source.id.clone(), 0, 0);
+
+    let mut region1 = region(region_size, cell_size);
+    region1.source_ids.insert(source.id.clone());
+    region1.position.x = EvenUsize::truncating_new(4); // 二つ目のリージョンの描画位置は端から 4 pixel 分ずらす
+    region1.position.y = EvenUsize::truncating_new(4);
+    region1.top_border_pixels = EvenUsize::truncating_new(2);
+    region1.grid.rows = 1;
+    region1.grid.columns = 1;
+    region1.grid.assign_source(source.id.clone(), 0, 0);
+
+    let mut output_rx = VideoMixerThread::start(
+        error_flag.clone(),
+        layout(&[region0, region1], &[&source], output_size, None),
+        vec![input_rx],
+        stats.clone(),
+    );
+
+    // 入力映像フレームを送信する: 500 ms のフレームを二つ
+    // リサイズを発生させるために cell_size よりもサイズを大きくする
+    let frame_size = size(14, 12); // 2 pixel ずつ大きい
+    let input_frame0 = video_frame(&source, frame_size, ms(0), ms(500), 2);
+    let input_frame1 = video_frame(&source, frame_size, ms(500), ms(500), 4);
+    input_tx.send(input_frame0.clone());
+    input_tx.send(input_frame1.clone());
+    std::mem::drop(input_tx);
+
+    // 合成結果を取得する
+    for i in 0..total_duration.as_millis() / OUTPUT_FRAME_DURATION.as_millis() {
+        let frame = output_rx.recv().expect("failed to receive output frame");
+        assert_eq!(frame.width.get(), output_size.width);
+        assert_eq!(frame.height.get(), output_size.height);
+        assert_eq!(frame.timestamp, OUTPUT_FRAME_DURATION * i as u32);
+        assert_eq!(frame.duration, OUTPUT_FRAME_DURATION);
+
+        if i < 3 {
+            // ここまでは最初の入力フレームのデータが使われる
+            let expected = grayscale_image([
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0],
+                [0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            ]);
+            assert_eq!(frame.data, expected);
+        } else {
+            // ここからは次の入力フレームのデータが使われる
+            let expected = grayscale_image([
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0],
+                [0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+                [0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            ]);
+            assert_eq!(frame.data, expected);
+        }
+    }
+
+    // 全ての出力を取得した
+    assert!(output_rx.recv().is_none());
+
+    // エラーは発生していない
+    assert!(!error_flag.get());
+
+    // 統計情報を確認する
+    stats.with_lock(|stats| {
+        let stats = video_mixer_stats(stats);
+
+        assert!(!stats.error);
+        assert_eq!(stats.total_input_video_frame_count, 2);
+        assert_eq!(stats.total_output_video_frame_count, 5);
+        assert_eq!(stats.total_output_video_frame_seconds.get(), ms(1000));
+        assert_eq!(stats.total_trimmed_video_frame_count, 0);
+    });
+}
+
 /// トリム期間（入力ソースが存在しなくて合成結果から除去される期間）がある場合のテスト
 #[test]
 fn mix_with_trim() -> orfail::Result<()> {
