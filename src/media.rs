cea8eff5413e029f491714e9b2f61547b22ca894
Takeru Ohta <phjgt308@gmail.com>
2025-08-15 16:37:19 +0900

    Add MediaStreamIdGenerator for generating unique media stream IDs

--- src/media.rs
+++ src/media.rs
@@ -7,6 +7,21 @@ use crate::video::VideoFrame;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct MediaStreamId(u64);
 
+#[derive(Debug)]
+pub struct MediaStreamIdGenerator(MediaStreamId);
+
+impl MediaStreamIdGenerator {
+    pub fn new() -> Self {
+        Self(MediaStreamId(0))
+    }
+
+    pub fn next_media_stream_id(&mut self) -> MediaStreamId {
+        let id = self.0;
+        self.0.0 += 1;
+        id
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum MediaSample {
     Audio(AudioData),
