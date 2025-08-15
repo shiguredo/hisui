5562deb17bbcc55d5c3f88c1c16fdf5ba30b0465
Takeru Ohta <phjgt308@gmail.com>
2025-08-15 16:30:26 +0900

    MediaProcessorトレイトのAPIを統合型の入出力に変更し構造体・列挙型を追加

--- src/processor.rs
+++ src/processor.rs
@@ -1,7 +1,26 @@
+use crate::media::{MediaStreamId, SharedMediaSample};
 use crate::stats::ProcessorStats;
 
 pub trait MediaProcessor {
-    fn process_input(&mut self);
-    fn generate_output(&mut self);
+    fn process(&mut self, input: MediaProcessorInput) -> orfail::Result<()>;
+    fn poll_output(&mut self) -> orfail::Result<MediaProcessorOutput>;
     fn stats(&self) -> ProcessorStats;
+}
+
+#[derive(Debug)]
+pub struct MediaProcessorInput {
+    pub stream_id: MediaStreamId,
+    pub sample: Option<SharedMediaSample>,
+}
+
+#[derive(Debug)]
+pub enum MediaProcessorOutput {
+    Processed {
+        stream_id: MediaStreamId,
+        sample: SharedMediaSample,
+    },
+    Pending {
+        awaiting_stream_id: MediaStreamId,
+    },
+    Finished,
 }
