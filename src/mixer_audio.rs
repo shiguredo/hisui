629dbb7a044306cec38a5d2d230ac8da025329c5
Takeru Ohta <phjgt308@gmail.com>
2025-08-20 11:19:49 +0900

    AudioMixer を MediaProcessor インターフェースに適合するよう修正、統計処理を統一

--- src/mixer_audio.rs
+++ src/mixer_audio.rs
@@ -12,7 +12,6 @@ use crate::{
     channel::{self, ErrorFlag},
     layout::Layout,
     media::{MediaSample, MediaStreamId},
-    metadata::SourceId,
     processor::{MediaProcessor, MediaProcessorInput, MediaProcessorOutput, MediaProcessorSpec},
     stats::{AudioMixerStats, ProcessorStats, Seconds, SharedStats},
 };
@@ -22,11 +21,9 @@ const MIXED_AUDIO_DATA_SAMPLES: usize = 960;
 
 #[derive(Debug)]
 pub struct AudioMixerThread {
-    layout: Layout,
     input_rxs: Vec<AudioDataReceiver>,
     output_tx: AudioDataSyncSender,
-    input_sample_queues: HashMap<SourceId, VecDeque<(i16, i16)>>,
-    stats: AudioMixerStats,
+    inner: AudioMixer,
 }
 
 impl AudioMixerThread {
@@ -37,185 +34,61 @@ impl AudioMixerThread {
         shared_stats: SharedStats,
     ) -> AudioDataReceiver {
         let (tx, rx) = channel::sync_channel();
+        let input_stream_ids = (0..input_rxs.len())
+            .map(|i| MediaStreamId::new(i as u64))
+            .collect();
+        let output_stream_id = MediaStreamId::new(input_rxs.len() as u64);
+        let inner = AudioMixer::new(layout, input_stream_ids, output_stream_id);
         let mut this = Self {
-            layout,
             input_rxs,
             output_tx: tx,
-            input_sample_queues: HashMap::new(),
-            stats: AudioMixerStats::default(),
+            inner,
         };
         std::thread::spawn(move || {
             log::debug!("audio mixer started");
             if let Err(e) = this.run().or_fail() {
                 error_flag.set();
-                this.stats.error.set(true);
+                this.inner.stats.error.set(true);
                 log::error!("failed to mix audio sources: {e}");
             }
             log::debug!("audio mixer finished");
 
             shared_stats.with_lock(|stats| {
-                stats
-                    .processors
-                    .push(ProcessorStats::AudioMixer(this.stats));
+                stats.processors.push(this.inner.spec().stats);
             });
         });
         rx
     }
 
-    fn next_input_timestamp(&self) -> Duration {
-        Duration::from_secs(
-            self.stats.total_output_sample_count.get()
-                + self.stats.total_trimmed_sample_count.get(),
-        ) / SAMPLE_RATE as u32
-    }
-
-    fn next_output_timestamp(&self) -> Duration {
-        Duration::from_secs(self.stats.total_output_sample_count.get()) / SAMPLE_RATE as u32
-    }
-
     fn run(&mut self) -> orfail::Result<()> {
-        while let Some(data) = self.next_data().or_fail()? {
-            if !self.output_tx.send(data) {
-                // 受信側がすでに閉じている場合にはこれ以上処理しても仕方がないので終了する
-                log::info!("receiver of mixed audio stream has been closed");
-                break;
+        loop {
+            match self.inner.process_output().or_fail()? {
+                MediaProcessorOutput::Finished => break,
+                MediaProcessorOutput::Processed { sample, .. } => {
+                    let data = sample.expect_audio_data().or_fail()?;
+
+                    // 今の実装ではここの参照カウントは常に 1 となるので必ず成功する
+                    // [NOTE] 将来的には `run()` 自体がなくなるので、このコードも暫定的なもの
+                    let data = std::sync::Arc::into_inner(data).or_fail()?;
+
+                    if !self.output_tx.send(data) {
+                        // 受信側がすでに閉じている場合にはこれ以上処理しても仕方がないので終了する
+                        log::info!("receiver of mixed audio stream has been closed");
+                        break;
+                    }
+                }
+                MediaProcessorOutput::Pending { awaiting_stream_id } => {
+                    let i = awaiting_stream_id.get() as usize;
+                    let input = if let Some(data) = self.input_rxs[i].recv() {
+                        MediaProcessorInput::audio_data(awaiting_stream_id, data)
+                    } else {
+                        MediaProcessorInput::eos(awaiting_stream_id)
+                    };
+                    self.inner.process_input(input).or_fail()?;
+                }
             }
         }
         Ok(())
-    }
-
-    fn fill_input_queue(
-        &mut self,
-        now: Duration,
-        input_rx: &mut channel::Receiver<AudioData>,
-    ) -> orfail::Result<()> {
-        while let Some(data) = input_rx.peek() {
-            let source_id = data.source_id.as_ref().or_fail()?;
-
-            if !self.input_sample_queues.contains_key(source_id) && now < data.timestamp {
-                // まだ再生時刻に達していない
-                break;
-            } else if !self.input_sample_queues.contains_key(source_id) {
-                // 再生時刻に達した
-                //
-                // 以後は、データのタイムスタンプにギャップがあったとしても
-                // 連続しているものとして扱う
-                // (Chrome を含む多くのブラウザがこの挙動なのと、
-                // ギャップ部分のハンドリングは Sora 側の責務であるため。
-                // 下手に Hisui 側でハンドリングしてしまうと、ギャップが
-                // 極端に大きいためにあえて Sora がそのまま放置した区間を
-                // 埋めようとしてディスクやメモリを食いつぶしてしまう恐れがある)
-                self.input_sample_queues
-                    .insert(source_id.clone(), VecDeque::new());
-            }
-
-            // サンプルキューに要素を追加する
-            //
-            // 想定外の入力が来ていないかを念のためにチェックする
-            // (format と stereo については stereo_samples() の中でチェックしている)
-            (data.sample_rate == SAMPLE_RATE).or_fail()?;
-
-            let queue = self.input_sample_queues.get_mut(source_id).or_fail()?;
-            queue.extend(data.stereo_samples().or_fail()?);
-
-            // 処理した要素を取りだす
-            let _ = input_rx.recv();
-            self.stats.total_input_audio_data_count.add(1);
-
-            if queue.len() >= MIXED_AUDIO_DATA_SAMPLES {
-                // 次の合成処理に必要な分のサンプルは溜った
-                break;
-            }
-        }
-        Ok(())
-    }
-
-    fn next_data(&mut self) -> orfail::Result<Option<AudioData>> {
-        let mut now = self.next_input_timestamp();
-        while self.layout.is_in_trim_span(now) {
-            self.stats
-                .total_trimmed_sample_count
-                .add(MIXED_AUDIO_DATA_SAMPLES as u64);
-            now = self.next_input_timestamp();
-        }
-
-        for mut input_rx in std::mem::take(&mut self.input_rxs) {
-            self.fill_input_queue(now, &mut input_rx).or_fail()?;
-            if input_rx.peek().is_some() {
-                self.input_rxs.push(input_rx);
-            }
-        }
-
-        if self.is_eos() {
-            // 全部のソースが終端に達した
-            return Ok(None);
-        }
-
-        let (result, elapsed) = Seconds::elapsed(|| self.mix_next_audio_data().or_fail().map(Some));
-        self.stats.total_processing_seconds.add(elapsed);
-        result
-    }
-
-    fn is_eos(&self) -> bool {
-        self.input_rxs.is_empty()
-            && self
-                .input_sample_queues
-                .values()
-                .all(|queue| queue.is_empty())
-    }
-
-    fn mix_next_audio_data(&mut self) -> orfail::Result<AudioData> {
-        let timestamp = self.next_output_timestamp();
-
-        let bytes_per_sample = CHANNELS as usize * 2; // i16 で表現するので *2
-        let mut mixed_samples = Vec::with_capacity(MIXED_AUDIO_DATA_SAMPLES * bytes_per_sample);
-
-        let mut filled = true; // 無音補完されたかどうか
-        for _ in 0..MIXED_AUDIO_DATA_SAMPLES {
-            let mut acc_left = 0;
-            let mut acc_right = 0;
-            for queue in self.input_sample_queues.values_mut() {
-                let Some((left, right)) = queue.pop_front() else {
-                    continue;
-                };
-                acc_left += left as i32;
-                acc_right += right as i32;
-                filled = false;
-            }
-
-            let left = acc_left.clamp(i16::MIN as i32, i16::MAX as i32) as i16;
-            let right = acc_right.clamp(i16::MIN as i32, i16::MAX as i32) as i16;
-
-            mixed_samples.extend_from_slice(&left.to_be_bytes());
-            mixed_samples.extend_from_slice(&right.to_be_bytes());
-        }
-
-        self.stats.total_output_audio_data_count.add(1);
-        self.stats
-            .total_output_audio_data_seconds
-            .add(Seconds::new(MIXED_AUDIO_DATA_DURATION));
-        self.stats
-            .total_output_sample_count
-            .add(MIXED_AUDIO_DATA_SAMPLES as u64);
-        if filled {
-            self.stats
-                .total_output_filled_sample_count
-                .add(MIXED_AUDIO_DATA_SAMPLES as u64);
-        }
-
-        Ok(AudioData {
-            // 以下は固定値
-            source_id: None, // 合成後は常に None になる
-            format: AudioFormat::I16Be,
-            stereo: true, // Hisui では音声は常にステレオとして扱う
-            sample_rate: SAMPLE_RATE,
-            duration: MIXED_AUDIO_DATA_DURATION,
-            sample_entry: None, // 生データにはサンプルエントリーはない
-
-            // 以下は合成結果に応じた値
-            data: mixed_samples,
-            timestamp,
-        })
     }
 }
 
@@ -378,18 +251,20 @@ impl MediaProcessor for AudioMixer {
 
         // 入力が不足しているソースがないかをチェックする
         for (input_stream_id, input_stream) in &self.input_streams {
-            if input_stream.start_timestamp.is_none_or(|t| t < now) {
-                // 開始時刻に達していない
-                continue;
-            }
             if input_stream.eos {
                 // これ以上新しい入力は来ないので待たない
                 continue;
             }
-            if input_stream.sample_queue.len() < MIXED_AUDIO_DATA_SAMPLES {
+            if input_stream.start_timestamp.is_some_and(|t| t < now) {
+                // 開始時刻に達していない
+                continue;
+            }
+            if input_stream.start_timestamp.is_none()
+                || input_stream.sample_queue.len() < MIXED_AUDIO_DATA_SAMPLES
+            {
                 // 次の合成に必要なサンプル数が足りないので待つ
                 return Ok(MediaProcessorOutput::Pending {
-                    awaiting_stream_id: input_stream_id,
+                    awaiting_stream_id: *input_stream_id,
                 });
             }
         }
@@ -400,12 +275,11 @@ impl MediaProcessor for AudioMixer {
             .values()
             .all(|s| s.eos && s.sample_queue.is_empty());
         if eos {
-            return Ok(MediaProcessorOutput::Finishied);
+            return Ok(MediaProcessorOutput::Finished);
         }
 
         // 合成
-        let (result, elapsed) =
-            Seconds::elapsed(|| self.mix_next_audio_data(now).or_fail().map(Some));
+        let (result, elapsed) = Seconds::elapsed(|| self.mix_next_audio_data(now).or_fail());
 
         // TODO: プロセッサ実行スレッドの導入タイミングで、時間計測はそっちに移動する
         self.stats.total_processing_seconds.add(elapsed);
