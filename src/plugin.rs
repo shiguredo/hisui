0ce696908a321da331d31b997e669f1bfa88e04e
Takeru Ohta <phjgt308@gmail.com>
2025-09-03 11:57:29 +0900

    feat: PluginCommandにstart()メソッドとプロセス実行機能を実装

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+use orfail::OrFail;
+
 use crate::media::MediaStreamId;
 
 #[derive(Debug)]
@@ -9,9 +11,38 @@ pub struct PluginCommand {
     pub input_stream_ids: Vec<MediaStreamId>,
 }
 
+impl PluginCommand {
+    pub fn start(&self) -> orfail::Result<PluginCommandProcessor> {
+        let mut process = std::process::Command::new(&self.command)
+            .args(&self.args)
+            .stdin(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .or_fail_with(|e| format!("failed to start plugin command: {e}"))?;
+
+        let stdin = process
+            .stdin
+            .take()
+            .or_fail_with(|()| "failed to get stdin handle".to_owned())?;
+
+        let stdout = process
+            .stdout
+            .take()
+            .or_fail_with(|()| "failed to get stdout handle".to_owned())?;
+
+        Ok(PluginCommandProcessor {
+            process,
+            stdin,
+            stdout,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct PluginCommandProcessor {
     process: std::process::Child,
+    stdin: std::process::ChildStdin,
+    stdout: std::process::ChildStdout,
 }
 
 impl Drop for PluginCommandProcessor {
