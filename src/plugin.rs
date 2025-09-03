8a622009d19777911ab99091026fff3631cf499c
Takeru Ohta <phjgt308@gmail.com>
2025-09-03 15:09:40 +0900

    feat: MediaStreamIdにnojson traitとJSON-RPC通信機能を実装

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,10 +1,10 @@
-use std::io::BufWriter;
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
 use orfail::OrFail;
 
 use crate::audio::AudioData;
-use crate::media::MediaStreamId;
+use crate::media::{MediaSample, MediaStreamId};
 use crate::processor::{
     MediaProcessor, MediaProcessorInput, MediaProcessorOutput, MediaProcessorSpec,
 };
@@ -56,6 +56,33 @@ pub struct PluginCommandProcessor {
     next_request_id: u64,
 }
 
+impl PluginCommandProcessor {
+    fn cast<T>(
+        &mut self,
+        notification: &JsonRpcRequest<T>,
+        payload: Option<&[u8]>,
+    ) -> orfail::Result<()>
+    where
+        T: nojson::DisplayJson,
+    {
+        let notification = nojson::Json(notification).to_string();
+        writeln!(self.stdin, "Content-Length: {}", notification.len()).or_fail()?;
+        writeln!(self.stdin, "Content-Type: application/json").or_fail()?;
+        writeln!(self.stdin).or_fail()?;
+        write!(self.stdin, "{notification}").or_fail()?;
+
+        if let Some(payload) = payload {
+            writeln!(self.stdin, "Content-Length: {}", payload.len()).or_fail()?;
+            writeln!(self.stdin, "Content-Type: application/octet-stream").or_fail()?;
+            writeln!(self.stdin).or_fail()?;
+            self.stdin.write_all(payload).or_fail()?;
+        }
+
+        self.stdin.flush().or_fail()?;
+        Ok(())
+    }
+}
+
 impl MediaProcessor for PluginCommandProcessor {
     fn spec(&self) -> MediaProcessorSpec {
         MediaProcessorSpec {
@@ -66,9 +93,18 @@ impl MediaProcessor for PluginCommandProcessor {
     }
 
     fn process_input(&mut self, input: MediaProcessorInput) -> orfail::Result<()> {
-        if let Some(sample) = input.sample {
-        } else {
-            self.input_stream_ids.retain(|id| *id != input.stream_id);
+        match input.sample {
+            None => {
+                self.input_stream_ids.retain(|id| *id != input.stream_id);
+
+                let req = JsonRpcRequest::notification(
+                    "notify_eos",
+                    nojson::object(|f| f.member("stream_id", input.stream_id)),
+                );
+                self.cast(&req, None).or_fail()?;
+            }
+            Some(MediaSample::Audio(_)) => {}
+            Some(MediaSample::Video(_)) => {}
         }
         Ok(())
     }
@@ -86,6 +122,41 @@ impl Drop for PluginCommandProcessor {
 }
 
 #[derive(Debug)]
+pub struct JsonRpcRequest<'a, T> {
+    method: &'a str,
+    id: Option<u64>,
+    params: T,
+}
+
+impl<'a, T> JsonRpcRequest<'a, T> {
+    pub fn notification(method: &'a str, params: T) -> Self {
+        Self {
+            method,
+            id: None,
+            params,
+        }
+    }
+}
+
+impl<'a, T> nojson::DisplayJson for JsonRpcRequest<'a, T>
+where
+    T: nojson::DisplayJson,
+{
+    fn fmt(&self, f: &mut nojson::JsonFormatter<'_, '_>) -> std::fmt::Result {
+        f.object(|f| {
+            f.member("jsonrpc", "2.0")?;
+            f.member("method", self.method)?;
+            if let Some(id) = self.id {
+                f.member("id", id)?;
+            }
+            f.member("params", &self.params)?;
+            Ok(())
+        })
+    }
+}
+
+/*
+#[derive(Debug)]
 pub enum JsonRpcRequest<'a> {
     NotifyAudioData {
         stream_id: MediaStreamId,
@@ -102,6 +173,7 @@ pub enum JsonRpcRequest<'a> {
         request_id: u64,
     },
 }
+*/
 
 #[derive(Debug)]
 pub enum PollOutputResponse {
