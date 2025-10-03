768da0698e7a3b2541e4f77349ae4481fb79ee1a
Takeru Ohta <phjgt308@gmail.com>
2025-10-03 14:52:06 +0900

    feat: nvcodecデコーダーの実装を追加

--- src/decoder_nvcodec.rs
+++ src/decoder_nvcodec.rs
@@ -1,1 +1,97 @@
+use std::collections::VecDeque;
 
+use orfail::OrFail;
+
+use crate::video::{VideoFormat, VideoFrame};
+
+#[derive(Debug)]
+pub struct NvcodecDecoder {
+    inner: shiguredo_nvcodec::Decoder,
+    input_queue: VecDeque<VideoFrame>,
+    output_queue: VecDeque<VideoFrame>,
+}
+
+impl NvcodecDecoder {
+    pub fn new_h265() -> orfail::Result<Self> {
+        log::debug!("create nvcodec(H265) decoder");
+        Ok(Self {
+            inner: shiguredo_nvcodec::Decoder::new_h265().or_fail()?,
+            input_queue: VecDeque::new(),
+            output_queue: VecDeque::new(),
+        })
+    }
+
+    pub fn decode(&mut self, frame: &VideoFrame) -> orfail::Result<()> {
+        (frame.format == VideoFormat::H265).or_fail()?;
+
+        self.inner.decode(&frame.data).or_fail()?;
+        self.input_queue.push_back(frame.to_stripped());
+        self.handle_decoded_frames().or_fail()?;
+        Ok(())
+    }
+
+    pub fn finish(&mut self) -> orfail::Result<()> {
+        self.inner.finish().or_fail()?;
+        self.handle_decoded_frames().or_fail()?;
+        Ok(())
+    }
+
+    fn handle_decoded_frames(&mut self) -> orfail::Result<()> {
+        while let Some(nv12_frame) = self.inner.next_frame() {
+            let input_frame = self.input_queue.pop_front().or_fail()?;
+
+            // NV12 から I420 への変換
+            let width = nv12_frame.width();
+            let height = nv12_frame.height();
+
+            // I420 用のバッファを確保
+            let y_size = width * height;
+            let uv_width = width.div_ceil(2);
+            let uv_height = height.div_ceil(2);
+            let uv_size = uv_width * uv_height;
+            let total_size = y_size + uv_size * 2;
+
+            let mut i420_data = vec![0u8; total_size];
+            let (y_plane, rest) = i420_data.split_at_mut(y_size);
+            let (u_plane, v_plane) = rest.split_at_mut(uv_size);
+
+            // libyuv を使って NV12 から I420 に変換
+            let src = shiguredo_libyuv::Nv12Planes {
+                y: nv12_frame.y_plane(),
+                y_stride: nv12_frame.y_stride(),
+                uv: nv12_frame.uv_plane(),
+                uv_stride: nv12_frame.uv_stride(),
+            };
+
+            let mut dst = shiguredo_libyuv::I420PlanesMut {
+                y: y_plane,
+                y_stride: width,
+                u: u_plane,
+                u_stride: uv_width,
+                v: v_plane,
+                v_stride: uv_width,
+            };
+
+            let size = shiguredo_libyuv::ImageSize::new(width, height);
+            shiguredo_libyuv::nv12_to_i420(&src, &mut dst, size).or_fail()?;
+
+            // I420 VideoFrame を作成
+            self.output_queue.push_back(VideoFrame::new_i420(
+                input_frame,
+                width,
+                height,
+                y_plane,
+                u_plane,
+                v_plane,
+                width,
+                uv_width,
+                uv_width,
+            ));
+        }
+        Ok(())
+    }
+
+    pub fn next_decoded_frame(&mut self) -> Option<VideoFrame> {
+        self.output_queue.pop_front()
+    }
+}
