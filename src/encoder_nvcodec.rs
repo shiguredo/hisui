c6ab40814ab3dc46b396b5a0c6501dd45e0cadaf
Takeru Ohta <phjgt308@gmail.com>
2025-10-07 13:02:46 +0900

    refactor: 未使用のimportを削除

--- src/encoder_nvcodec.rs
+++ src/encoder_nvcodec.rs
@@ -5,8 +5,7 @@ use shiguredo_mp4::boxes::SampleEntry;
 
 use crate::{
     encoder::VideoEncoderOptions,
-    types::EvenUsize,
-    video::{FrameRate, VideoFormat, VideoFrame},
+    video::{VideoFormat, VideoFrame},
     video_av1, video_h264, video_h265,
 };
 
