794779826e45adf3627a316110d8e6c6904a75a8
Takeru Ohta <phjgt308@gmail.com>
2025-12-04 16:03:19 +0900

    feat: NVENC エラーステータスの詳細メッセージ表示を追加

--- crates/shiguredo_nvcodec/src/lib.rs
+++ crates/shiguredo_nvcodec/src/lib.rs
@@ -53,15 +53,106 @@ impl Error {
             Err(Self::new(status, function, reason))
         }
     }
+
+    fn status_to_message(status: u32) -> Option<&'static str> {
+        match status {
+            sys::_NVENCSTATUS_NV_ENC_SUCCESS => Some("Encoding completed successfully"),
+            sys::_NVENCSTATUS_NV_ENC_ERR_NO_ENCODE_DEVICE => {
+                Some("No encode capable devices were detected")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_UNSUPPORTED_DEVICE => {
+                Some("The devices passed by the client is not supported")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_ENCODERDEVICE => {
+                Some("The encoder device supplied by the client is not valid")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_DEVICE => {
+                Some("Device passed to the API call is invalid")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_DEVICE_NOT_EXIST => Some(
+                "Device passed to the API call is no longer available and needs to be reinitialized",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_PTR => {
+                Some("One or more of the pointers passed to the API call is invalid")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_EVENT => {
+                Some("Completion event passed in NvEncEncodePicture() call is invalid")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_PARAM => {
+                Some("One or more of the parameter passed to the API call is invalid")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_CALL => {
+                Some("An API call was made in wrong sequence/order")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_OUT_OF_MEMORY => Some(
+                "The API call failed because it was unable to allocate enough memory to perform the requested operation",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_ENCODER_NOT_INITIALIZED => Some(
+                "The encoder has not been initialized with NvEncInitializeEncoder() or initialization has failed",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_UNSUPPORTED_PARAM => {
+                Some("An unsupported parameter was passed by the client")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_LOCK_BUSY => Some(
+                "NvEncLockBitstream() failed to lock the output buffer. Retry after few milliseconds",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_NOT_ENOUGH_BUFFER => Some(
+                "The size of the user buffer passed by the client is insufficient for the requested operation",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_INVALID_VERSION => {
+                Some("An invalid struct version was used by the client")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_MAP_FAILED => {
+                Some("NvEncMapInputResource() API failed to map the client provided input resource")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_NEED_MORE_INPUT => Some(
+                "Encode driver requires more input buffers to produce an output bitstream. This is not a fatal error",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_ENCODER_BUSY => Some(
+                "The HW encoder is busy encoding and is unable to encode the input. Retry after few milliseconds",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_EVENT_NOT_REGISTERD => Some(
+                "The completion event passed in NvEncEncodePicture() API has not been registered with encoder driver using NvEncRegisterAsyncEvent()",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_GENERIC => Some("An unknown internal error has occurred"),
+            sys::_NVENCSTATUS_NV_ENC_ERR_INCOMPATIBLE_CLIENT_KEY => Some(
+                "The client is attempting to use a feature that is not available for the license type for the current system",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_UNIMPLEMENTED => Some(
+                "The client is attempting to use a feature that is not implemented for the current version",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_RESOURCE_REGISTER_FAILED => {
+                Some("NvEncRegisterResource API failed to register the resource")
+            }
+            sys::_NVENCSTATUS_NV_ENC_ERR_RESOURCE_NOT_REGISTERED => Some(
+                "The client is attempting to unregister a resource that has not been successfully registered",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_RESOURCE_NOT_MAPPED => Some(
+                "The client is attempting to unmap a resource that has not been successfully mapped",
+            ),
+            sys::_NVENCSTATUS_NV_ENC_ERR_NEED_MORE_OUTPUT => Some(
+                "Encode driver requires more output buffers to write an output bitstream. This is not a fatal error",
+            ),
+
+            // 以下の場合は、有益な情報を出すことができないので None にする
+            sys::cudaError_enum_CUDA_SUCCESS => None,
+            sys::cudaError_enum_CUDA_ERROR_UNKNOWN => None,
+            _ => None,
+        }
+    }
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}() failed: status={}, reason={}",
-            self.function, self.status, self.reason
-        )?;
+        if let Some(message) = Self::status_to_message(self.status) {
+            write!(
+                f,
+                "{}() failed: {} ([status={}] {message})",
+                self.function, self.reason, self.status
+            )?;
+        } else {
+            write!(f, "{}() failed: {}", self.function, self.reason)?;
+        }
         Ok(())
     }
 }
