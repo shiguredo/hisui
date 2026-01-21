0a7446db926522341bf3d382b3467c568e7831dd
Takeru Ohta <phjgt308@gmail.com>
2026-01-21 13:06:52 +0900

    feat: MP4 リーダーに動画フレーム読み込み機能を実装する

--- src/reader_mp4.rs
+++ src/reader_mp4.rs
@@ -8,7 +8,7 @@ use std::{
 
 use orfail::OrFail;
 use shiguredo_mp4::{
-    BoxHeader, Decode,
+    BoxHeader, Decode, TrackKind,
     aux::SampleTableAccessor,
     boxes::{HdlrBox, MoovBox, SampleEntry, StblBox, TrakBox},
     demux::Mp4FileDemuxer,
@@ -23,8 +23,12 @@ use crate::{
 
 #[derive(Debug)]
 pub struct Mp4VideoReader2 {
+    file: File,
     demuxer: Mp4FileDemuxer,
     source_id: SourceId,
+    format: VideoFormat,
+    width: usize,
+    height: usize,
     stats: Mp4VideoReaderStats,
 }
 
@@ -58,14 +62,95 @@ impl Mp4VideoReader2 {
         }
 
         Ok(Self {
+            file,
             demuxer,
             source_id,
             stats,
+
+            // 後で更新されるので適当な初期値を設定しておく
+            format: VideoFormat::Vp8,
+            width: 0,
+            height: 0,
         })
     }
 
     pub fn stats(&self) -> &Mp4VideoReaderStats {
         &self.stats
+    }
+
+    fn next_frame(&mut self) -> orfail::Result<Option<VideoFrame>> {
+        let sample = 'next_sample: loop {
+            match self
+                .demuxer
+                .next_sample()
+                .or_fail_with(|e| format!("Read sample error: {e}"))?
+            {
+                None => return Ok(None),
+                Some(sample) if sample.track.kind != TrackKind::Video => {}
+                Some(sample) => break 'next_sample sample,
+            }
+        };
+
+        let sample_entry = sample.sample_entry.cloned();
+        if let Some(sample_entry) = &sample_entry {
+            // 新しいサンプルエントリーが来たのでハンドリングする
+            let (metadata, format) = match sample_entry {
+                SampleEntry::Avc1(b) => (&b.visual, VideoFormat::H264),
+                SampleEntry::Hev1(b) => (&b.visual, VideoFormat::H265),
+                SampleEntry::Hvc1(b) => (&b.visual, VideoFormat::H265),
+                SampleEntry::Vp08(b) => (&b.visual, VideoFormat::Vp8),
+                SampleEntry::Vp09(b) => (&b.visual, VideoFormat::Vp9),
+                SampleEntry::Av01(b) => (&b.visual, VideoFormat::Av1),
+                entry => {
+                    return Err(orfail::Failure::new(format!(
+                        "unsupported sample entry: {entry:?}"
+                    )));
+                }
+            };
+
+            self.format = format;
+            self.width = metadata.width as usize;
+            self.height = metadata.height as usize;
+        }
+
+        // サンプルデータを読み込む
+        let mut data = vec![0; sample.data_size];
+        self.file
+            .seek(SeekFrom::Start(sample.data_offset))
+            .or_fail_with(|e| format!("Seek error: {e}"))?;
+        self.file
+            .read_exact(&mut data)
+            .or_fail_with(|e| format!("Read error: {e}"))?;
+
+        // タイムスタンプを計算する
+        let timescale = sample.track.timescale.get();
+        let timestamp = Duration::from_secs(sample.timestamp) / timescale;
+        let duration = Duration::from_secs(sample.duration as u64) / timescale;
+
+        // 統計値を更新する
+        self.stats.total_sample_count.add(1);
+        self.stats.total_track_duration.set(timestamp + duration);
+        if self.stats.codec.get().is_none()
+            && let Some(name) = self.format.codec_name()
+        {
+            self.stats.codec.set(name);
+        }
+        self.stats.resolutions.insert(VideoResolution {
+            width: self.width,
+            height: self.height,
+        });
+
+        Ok(Some(VideoFrame {
+            source_id: Some(self.source_id.clone()),
+            sample_entry,
+            data,
+            format: self.format,
+            keyframe: sample.keyframe,
+            width: self.width,
+            height: self.height,
+            timestamp,
+            duration,
+        }))
     }
 }
 
@@ -73,15 +158,7 @@ impl Iterator for Mp4VideoReader2 {
     type Item = orfail::Result<VideoFrame>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self
-            .demuxer
-            .next_sample()
-            .transpose()?
-            .or_fail_with(|e| format!("Read sample error: {e}"))
-        {
-            Err(e) => Some(Err(e)),
-            Ok(sample) => todo!(),
-        }
+        self.next_frame().or_fail().transpose()
     }
 }
 
