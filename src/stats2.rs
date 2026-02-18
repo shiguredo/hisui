5ec9753dea00334e166db512c78c3e10f8c5bddc
Takeru Ohta <phjgt308@gmail.com>
2026-02-18 17:00:46 +0900

    feat: stats2 モジュールに Stats、LocalStats、StatsEntry、StatsCounter、Labels 構造体を追加する

--- src/stats2.rs
+++ src/stats2.rs
@@ -1,1 +1,32 @@
+use std::collections::BTreeMap;
+use std::sync::{Arc, Mutex, atomic::AtomicU64};
 
+#[derive(Debug)]
+pub struct Stats {
+    pub entries: BTreeMap<String, StatsEntry>,
+}
+
+impl Stats {}
+
+#[derive(Debug, Clone)]
+pub struct LocalStats {
+    pub global: Arc<Mutex<Stats>>,
+    pub local_common_labels: Labels,
+    pub local_entires: BTreeMap<String, StatsEntry>,
+}
+
+impl LocalStats {}
+
+#[derive(Debug, Clone)]
+pub enum StatsEntry {
+    Counter(StatsCounter),
+}
+
+#[derive(Debug, Clone)]
+pub struct StatsCounter {
+    pub labels: Labels,
+    pub value: Arc<AtomicU64>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Labels(pub Arc<Mutex<BTreeMap<String, String>>>);
