46c9fece48efb060490b57dbbacee78816bfdc29
Takeru Ohta <phjgt308@gmail.com>
2026-02-19 10:18:03 +0900

    refactor: Stats 構造体を再設計し LocalStats を廃止する

--- src/stats2.rs
+++ src/stats2.rs
@@ -1,21 +1,22 @@
 use std::collections::BTreeMap;
 use std::sync::{Arc, Mutex, atomic::AtomicU64};
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
 pub struct Stats {
-    pub entries: BTreeMap<String, StatsEntry>,
-}
-
-impl Stats {}
-
-#[derive(Debug, Clone)]
-pub struct LocalStats {
-    pub global: Arc<Mutex<Stats>>,
+    pub entries: Arc<Mutex<BTreeMap<String, StatsEntry>>>,
     pub local_common_labels: Labels,
-    pub local_entires: BTreeMap<String, StatsEntry>,
+    pub local_entries: BTreeMap<String, StatsEntry>, // cache?
 }
 
-impl LocalStats {}
+impl Stats {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn set_local_common_label(&mut self, name: &str, value: &str) {
+        self.local_common_labels.insert(name, value);
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum StatsEntry {
@@ -28,5 +29,11 @@ pub struct StatsCounter {
     pub value: Arc<AtomicU64>,
 }
 
-#[derive(Debug, Clone)]
-pub struct Labels(pub Arc<Mutex<BTreeMap<String, String>>>);
+#[derive(Debug, Default, Clone)]
+pub struct Labels(BTreeMap<String, String>);
+
+impl Labels {
+    fn insert(&mut self, name: &str, value: &str) {
+        self.0.insert(name.to_owned(), value.to_owned());
+    }
+}
