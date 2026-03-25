f59b527fdd1e49891536b998cb0bab50ddc8ed4d
Takeru Ohta <phjgt308@gmail.com>
2026-03-25 10:34:13 +0900

    refactor: 未使用の MediaStream 型エイリアスを削除する

--- src/media.rs
+++ src/media.rs
@@ -1,5 +1,4 @@
 use std::sync::Arc;
-use std::sync::mpsc::{Receiver, SyncSender};
 use std::time::Duration;
 
 use crate::audio::AudioFrame;
@@ -55,6 +54,3 @@ impl MediaFrame {
         Self::Video(Arc::new(frame))
     }
 }
-
-pub type MediaStreamReceiver = Receiver<MediaFrame>;
-pub type MediaStreamSyncSender = SyncSender<MediaFrame>;
